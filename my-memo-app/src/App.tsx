a1173aa2c6ac62cf2eba9eec54137259bedd4a56
matsumoto <matsumoto.renraku@gmail.com>
2025-09-10 11:54:23 +0900

    メモ機能実装

--- my-memo-app/src/App.tsx
+++ my-memo-app/src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import type { Memo } from './types/memo.ts';
 import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [memos, setMemos] = useState<Memo[]>([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  // メモ追加
+  const addMemo = () => {
+    if (!title && !content) return;
+    const newMemo: Memo = {
+      id: Date.now(),
+      title,
+      content,
+      createdAt: new Date().toISOString(),
+    };
+    setMemos([...memos, newMemo]);
+    setTitle("");
+    setContent("");
+  }
+
+  //　メモ削除
+  const deleteMemo = (id: number) => {
+    setMemos(memos.filter((memo) => memo.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ padding: "20px" }}>
+      <h1>メモアプリ</h1>
+      
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="タイトル"
+        style={{ display: "block", marginBottom: "5px" }}
+       />
+       <textarea
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="内容"
+        style={{ display: "block", marginBottom: "5px", width: "300px", height: "80px" }}
+      />
+      <button onClick={addMemo}>追加</button>
 
-export default App
+      <ul>
+        {memos.map((memo) => (
+          <li key={memo.id} style={{ margin: "10px 0", border: "1px solid #ccc", padding: "5px" }}>
+            <div><strong>{memo.title}</strong></div>
+            <div style={{ fontSize: "0.8em", color: "gray" }}>
+              {new Date(memo.createdAt).toLocaleString()}
+            </div>
+            <button onClick={() => deleteMemo(memo.id)}>削除</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+  );
+};
+
+export default App;
